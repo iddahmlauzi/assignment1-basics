f01eb1d420c35322b78d8fef66e5d26d9e892776
Iddah <iddah@stanford.edu>
2026-04-08 19:31:18 -0700

    Set up modal training

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -6,15 +6,13 @@ import wandb
 import time
 import torch
 import yaml
+from pathlib import Path
 from cs336_basics.data import get_batch
 from cs336_basics.model import TransformerLM
 from cs336_basics.optim import AdamW, clip_gradients, get_cosine_lr
 from cs336_basics.checkpoint import save_checkpoint, load_checkpoint
 from cs336_basics.loss import cross_entropy
-from pathlib import Path
 
-
-# TODO: How to set up Modal? This is annoying argh but figure out how to do this. --> Will do this later
 
 def train(args):
     
@@ -141,90 +139,6 @@ def train(args):
     print(f"Training complete. Saving final model to {checkpoint_dir}")
     save_checkpoint(model=model, optimizer=optimizer, iteration=num_steps, out=checkpoint_dir / "final_model.pt")
         
-
-if __name__ == "__main__":
-    
-    # Parse the yaml config
-    # Doing this to make runs easier (so I can just modify the config directly)
-    config_parser = argparse.ArgumentParser(add_help=False)
-    config_parser.add_argument("--config", type=str, help="Path to a yaml config file")
-    # So this will put the config file in args and everything else in remaining args
-    args, remaining_args = config_parser.parse_known_args()
-    
-    # Load the YAML config file
-    default_args = {}
-    if args.config:
-        with open(args.config, "r") as f:
-            default_args = yaml.safe_load(f)
-
-    parser = argparse.ArgumentParser()
-    # Model Hyperparameters: DEFAULTS FOR TINYSTORIES
-    parser.add_argument("--vocab_size", type=int, default=10000)
-    parser.add_argument("--context_length", type=int, default=256)
-    parser.add_argument("--d_model", type=int, default=512)
-    parser.add_argument("--d_ff", type=int, default=1344)
-    parser.add_argument("-theta","--rope_theta", type=float, default=10000)
-    parser.add_argument("--num_layers", type=int, default=4)
-    parser.add_argument("--num_heads", type=int, default=16)
-    
-    # Optimizer Hyperparameters: DEFAULTS DONE
-    parser.add_argument("--beta1", type=float, default=0.9)
-    parser.add_argument("--beta2", type=float, default=0.95)
-    parser.add_argument("--adam_eps", type=float, default=1e-8)
-    parser.add_argument("--weight_decay", type=float, default=1e-1)
-    
-    # LR Scheduler: DEFAULTS DONE
-    parser.add_argument("-max_lr","--max_learning_rate", type=float, default=1e-3,
-                        help="alpha_max, the maximum learning rate for cosine learning rate schedule (with warmup).")
-    parser.add_argument("-min_lr","--min_learning_rate", type=float, default=1e-4, 
-                        help="alpha_min, the minimum / final learning rate for the cosine learning rate schedule (with warmup).")
-    parser.add_argument("--warmup_iters", type=int, default=500, 
-                        help=" T_w, the number of iterations to linearly warm-up the learning rate.")
-    parser.add_argument("--cosine_cycle_iters", type=int, default=20000, 
-                        help="T_c, the number of cosine annealing iterations.")
-    
-    # Gradient Clipping: DEFAULTS DONE
-    parser.add_argument("--max_l2_norm", type=float, default=1,
-                        help="A positive value containing the maximum l2-norm.")
-    
-    # Training
-    parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--num_steps", type=int, default=20000)
-    parser.add_argument("--data_path", type=str, default=None)
-    parser.add_argument("--val_path", type=str, default=None)
-    parser.add_argument("--checkpoint_dir", type=str, default=None)
-    parser.add_argument("--checkpoint_path", type=str, 
-                        help="Path to the loaded checkpoint")
-    parser.add_argument("--device", type=str)
-    parser.add_argument("--eval_steps", type=int, default=500)
-    parser.add_argument("--eval_batches", type=int, default=20,
-                        help="Number of batches to sample from val dataset during evaluation")
-    parser.add_argument("--save_steps", type=int, default=2000)
-    parser.add_argument("--log_steps", type=int, default=100)
-    parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--total_tokens_processed", type=int)
-    
-    # Wandb specific argumsnets
-    parser.add_argument("--run_name", type=str, default=None)
-    parser.add_argument("--group_name", type=str, default=None)
-    
-    # Inject the yaml values to override the defaults that I mentioned above
-    parser.set_defaults(**default_args)
-    
-    # Then we can parse the rest of the commends (in case I want to add some in terminal)
-    args = parser.parse_args(remaining_args)
-    for argname in ["checkpoint_dir", "data_path", "val_path"]:
-        if getattr(args, argname) is None:
-            parser.error(f"the following arguments are required: --{argname} (provide via terminal or --config)")
-        
-    device = args.device or ("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
-    args.device = device
-    
-    wandb.init(project="CS336-Assignment1", 
-               name=args.run_name,
-               group=args.group_name,
-               config=vars(args))
-    train(args)
     
     
     
